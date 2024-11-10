8a42e5e80727a3fe8b61f21a6c4374616c41908f
Marcelo Cardoso <140186230+MarceloDEV05@users.noreply.github.com>
2024-11-10 12:42:32 -0300

    finalizando estilizações

--- main.js
+++ main.js
@@ -1,66 +1,34 @@
+const img = document.getElementById('img-central')
+const inputTask = document.getElementById('nameTask')
+const btnAddTask = document.getElementById('addTaskList')
+const modalTask = document.querySelector('.details-task')
+const openModalTask = document.getElementById('addTask')
+const closeModalTask = document.getElementById('cancelTask')
+const listTask = document.getElementById('list-task')
+
+let tasks = []
 
 
-let input = document.querySelector('.nameTask')
-let button = document.querySelector('.addTask')
-let ul = document.querySelector('ul')
-let alertMessage = document.getElementById('alert-message')
-let partImg = document.querySelector(".part-img")
-
-
-let list = []
-
-button.addEventListener('click', () => {
-
-    partImg.style.display = "none"
-
-    let liElement = document.createElement('li')
-    liElement.innerHTML = `<input id="check" type="checkbox"> ${input.value}`
-
-
-
-    input.style.borderColor = '#000'
-
-    ul.appendChild(liElement)
-
-    let btnElement = document.createElement('button')
-    btnElement.innerHTML = `<i class="fa-solid fa-trash"></i>`
-    btnElement.style.cssText = "background:none; margin-left: 320px; border:none"
-    btnElement.addEventListener('click', () => {
-        ul.removeChild(liElement)
-        ul.removeChild(btnElement)
-
-
-        if (ul.children.length === 0) {
-            partImg.style.display = "block"
-        }
-
-    })
-
-    
-    ul.appendChild(btnElement)
-
-    list.push(ul)
-
-
-
-    console.log(list)
-
-    if (input.value === "") {
-
-        input.style.cssText = "border:1px solid; border-color: #cf0e0e"
-
-        ul.removeChild(liElement)
-        ul.removeChild(btnElement)
-
-        alertMessage.style.display = "block"
-
-    }
-
-    input.value = ""
-
-  
-
+openModalTask.addEventListener('click', () => {
+    modalTask.style.display = "block"
 
 })
 
 
+closeModalTask.addEventListener('click', () => {
+    modalTask.style.display = 'none'
+})
+
+
+btnAddTask.addEventListener('click', () => {
+    img.style.display = 'none'
+
+    const li = document.createElement('li')
+    li.innerHTML = `<li>${inputTask.value}</li>`
+    console.log(li)
+
+    listTask.appendChild(li)
+    tasks.push(listTask)
+
+    modalTask.style.display="none"
+})
